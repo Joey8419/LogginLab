d39485461f2bc8469af98cc60e79d86cb05fe95a
Joey <joey@Zipcoders-MacBook-Pro-9.local>
2023-10-26 15:27:57 -0400

    Completed the LogginLab Portion

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -36,6 +36,14 @@ public class LogginLab {
     }
 
     // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
+     public boolean thresholdReached(Integer limit) {
+        if (this.threshold > limit) {
+            return true;
+        } else {
+            return false;
+        }
+
+     }
     // use thresholdExceeds for a pattern.
     // Write a test for the method in the Test class.
 }
